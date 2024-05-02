22244a69108ea6827cf6657856a1217112724ff3
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-05-02 15:02:44 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,64 @@
+let ir_waarde = 0
+let vooruit = 94
+let achteruit = 95
+let links = 93
+let rechts = 92
+let stop = 8
+basic.forever(function () {
+    if (ir_waarde == vooruit) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == links) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == achteruit) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == rechts) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == stop) {
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
+})
